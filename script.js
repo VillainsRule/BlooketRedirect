31ac260bc0da26dcb7e50a87a8672c641a02665f
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-11-04 06:53:07 -0400

    fix the script / css fixes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-document.querySelector('button').onclick = () => {
+document.querySelector('button').onclick = async () => {
     let content = document.querySelector('input').value;
     let hexCode = content.split('').map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');
     let percented = hexCode.replace(/(.{2})/g, '%$1');
     let code = 'https://id.blooket.com/login?n=%' + percented.slice(1);
-    navigator.clipboard.writeText(code);
-    console.log(`generated: ${code}`);
+    await navigator.clipboard.writeText(code);
     document.querySelector('input').value = '';
     alert('Code generated and copied to clipboard.');
 };
